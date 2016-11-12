62310077d62805911d3a5ee9c2057b2db0566f46
romainbebert <30secondstodraw@gmail.com>
2016-11-12 06:43:10 +0100

    Finite State Machine Implementation Ongoing

---
+++ Controllers/Engine.cpp
@@ -0,0 +1,50 @@
+#include <memory>
+#include "../Models/Board.hpp"
+#include "GameState.hpp"
+
+class Engine {
+
+	private :
+		Board _board
+
+		//States initialization 
+		GameState 		_state;
+		PressStartState _start;
+		SelectPosState 	_select;
+		PlayerTurnState _plturn;
+		NPCTurnState 	_npturn;
+		PauseState 		_pause;
+		GameOverState 	_gameover;
+	
+	public :
+		Engine::Engine() {
+			Board _board();
+			PressStartState _start();
+			SelectPosState 	_select(std::share_ptr(new _board));
+			PlayerTurnState _plturn(std::share_ptr(new _board));
+			NPCTurnState 	_npturn(std::share_ptr(new _board));
+			PauseState 		_pause(std::share_ptr(new _board));
+			GameOverState 	_gameover();
+			
+		}
+		
+		boolean Engine::handleEvent(Event e) {
+			
+		}
+		
+		void Engine::update(double deltaTime) {
+			_state.update(deltaTime);
+		}
+		
+		void Engine::render(sf::Window g) {
+			_state.render(g);
+		}
+	
+	protected :
+		void Engine::ChangeStateStart()		{ _state=_start; }
+		void Engine::ChangeStateSelect()	{ _state=_select; }
+		void Engine::ChangeStatePlTurn()	{ _state=_plturn; }
+		void Engine::ChangeStateNPTurn()	{ _state=_npturn; }
+		void Engine::ChangeStatePause()		{ _state=_pause; }
+		void Engine::ChangeStateGameOver()	{ _state=_gameover; }
+}
---
+++ Controllers/PlayerTurnState.cpp
@@ -0,0 +1,71 @@
+#include <memory>
+#include "../Models/Board.hpp"
+  
+class PlayerTurnState : GameState{
+
+    private :
+        std::shared_ptr<Board> board_;
+        BoardRenderer render_;
+        //BoardController controller_; is its own controller
+
+    public : 
+
+        /*To avoid instanciating multiple Controllers and renderers for both Player and NPC turn state
+        Also, Board is given as reference to allow for information sharing between the PlayerTurnState
+        the NPCTurnState and the PauseState, so multiple state can access and eventually change data*/
+        PlayerTurnState::PlayerTurnState(std::shared_ptr<Board> board, BoardRenderer render, Entity mc){
+            board_ = board; render_ = render;
+        }
+
+        bool PlayerTurnState::eventHandler(sf::Event e){
+            
+            switch(e.type){
+            	case sf::event::Closed : 
+
+
+                case sf::Event::KeyPressed :
+                    switch(e.key.code){
+                        case sf::Keyboard::1 :
+                            controller.useComp(1);
+                            break;
+                        case sf::Keyboard::2 :
+                            controller.useComp(2);
+                            break;
+                        case sf::Keyboard::3 :
+                            controller.useComp(3);
+                            break;
+                        case sf::Keyboard::4 :
+                            controller.useComp(4);
+                            break;
+                    }
+                    break;
+
+                case sf::Event::MouseButtonReleased :
+                    switch(event.mouseButton.button){
+                        case sf::Mouse::Right :
+                            controller.onClick();
+
+                        default : 
+                            break;
+                    }
+
+                // on ne traite pas les autres types d'évènements
+                default:
+                    break;
+            }
+
+            return false;
+        }
+
+        void PlayerTurnState::useComp(int i){
+
+        }
+
+        void PlayerTurnState::update(double delta){
+            controller_.update(delta);
+        }
+
+        void PlayerTurnState::render(sf::Window){
+            renderer_.render(sf::Window);
+        }
+};
